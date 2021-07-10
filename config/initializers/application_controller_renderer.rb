a9a827d0ba0e9dbcc90ea1e0f9b60db853069fe8
ykawa727 <y.aikawa727@gmail.com>
2021-07-10 12:36:03 +0000

    learned 3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a4336cc72cc18c0b370592321a794e878b0918289061c37f224d013c7badf03fd1b7805b431529c4a8e2d6eb14bb2eb185cb0800f8e83b9010bdc5235d4b2579'
