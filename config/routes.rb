044caf77c78f9132ab98471811bb2ec6aec66e75
ykawa727 <y.aikawa727@gmail.com>
2021-07-15 13:26:37 +0000

    my-page_learning 5

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :post_comments, only:[:create, :destroy]
     resource :favorites, only:[:create, :destroy]
   end
+  resources :users, only:[:show]
 
 end
